c3bf7f38a037e7397704b24c05c31df50e173232
Manuel Uc Nicoli <manueln0924@gmail.com>
2023-09-14 05:16:51 -0600

    Update IA

---
+++ IA_Practica2_UcNicoli.py
@@ -0,0 +1,28 @@
+import datetime
+
+# Problema 1
+# Crea una variable que contenga el año actual y muéstralo por pantalla.
+anio = datetime.date.today().year
+print("El año actual es: ", anio)
+
+# =============
+
+# Problema 2
+# Crea una variable con el valor de PI (3.14….) e imprímela.
+pi = 3.14159265359
+print("El valor de PI es: ", pi)
+
+# Problema 3
+# Crea una variable con el valor Verdadero (True) e imprímela.
+flag_true = True
+print("Valor Verdadero:", flag_true)
+
+# Problema 4
+# Crea una lista de 5 animales y muéstrala.
+animales = ["perro","gato","loro","cocodrilo","pato"]
+print("La lista de animales es: ", animales)
+
+# Problema 5
+# Crea una lista de 5 frutas y muestra solamente la última fruta.
+frutas = ["melon","sandia","manzana","durazno","fresas"]
+print("Última Fruta:", frutas[-1])
